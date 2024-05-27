8713ed01839c5e25429394e021f1c50c773bd092
K-Maryanne <maryanne.kariuki.k@gmail.com>
2024-05-27 10:32:23 +0300

    Update login and password rest

--- app/Views/login.php
+++ app/Views/login.php
@@ -29,10 +29,14 @@
                 </div>
                 <button type="submit" class="btn btn-primary">Login</button>
             </form>
+            <div class="mt-3">
+                <a href="<?= base_url('forgot-password') ?>" class="btn btn-link">Forgot Password?</a>
+            </div>
         </div>
     </div>
+
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
